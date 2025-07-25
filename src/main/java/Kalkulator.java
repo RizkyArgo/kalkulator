7f1933957b3650706ef47cf96ac5313519018fb5
RizkyArgo <rizkiargo@gmail.com>
2025-07-25 02:36:42 +0000

    tes commit

---
+++ src/main/java/Kalkulator.java
@@ -0,0 +1,27 @@
+public class Kalkulator{
+
+	public int tambah(int a,int b){
+		return a + b;
+	}
+
+	public int kurang(int a,int b){
+                return a - b;
+        }
+
+	public int kali(int a,int b){
+                return a * b;
+        }
+
+	public int bagi(int a,int b){
+		return a / b ;
+		}
+
+	public int modulus(int a,int b){
+                return a % b;
+        }
+
+	public int pangkat(int a,int b){
+                return a ^ b;
+        }
+
+	}
---
+++ src/test/java/KalkuTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Test;
+import static
+org.junit.jupiter.api.Assertions.*;
+
+public class KalkuTest{
+
+	@Test
+	public void tesTambah() {
+	Kalkulator kalku = new Kalkulator();
+	assertEquals(3, kalku.tambah(2, 1));
+	}
+
+	@Test
+        public void tesKurang() {
+        Kalkulator kalku = new Kalkulator();
+        assertEquals(1, kalku.kurang(2, 1));
+        }
+
+        @Test
+        public void tesKali() {
+        Kalkulator kalku = new Kalkulator();
+        assertEquals(3, kalku.kali(3, 1));
+        }
+
+        @Test
+        public void tesBagi() {
+        Kalkulator kalku = new Kalkulator();
+        assertEquals(3, kalku.tambah(6, 2));
+        }
+
+        @Test
+        public void tesModulus() {
+        Kalkulator kalku = new Kalkulator();
+        assertEquals(1, kalku.tambah(3, 2));
+        }
+
+        @Test
+        public void tesPangkat() {
+        Kalkulator kalku = new Kalkulator();
+        assertEquals(16, kalku.tambah(4, 2));
+        }
+
+}
